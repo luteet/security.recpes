31a9e05e6cadaf0279fc9a3e1e63004318f8415a
Roman Lukjanets <lukyanets14@gmail.com>
2023-05-31 12:34:34 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,181 @@
+
+const 
+	body = document.querySelector('body'),
+	html = document.querySelector('html'),
+	menu = document.querySelectorAll('.header__burger, .header__nav, body'),
+	burger = document.querySelector('.header__burger'),
+	header = document.querySelector('.header');
+
+
+// =-=-=-=-=-=-=-=-=-=- <Get-device-type> -=-=-=-=-=-=-=-=-=-=-
+
+const getDeviceType = () => {
+
+	const ua = navigator.userAgent;
+	if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+		return "tablet";
+	}
+
+	if (
+		/Mobile|iP(hone|od)|Android|BlackBerry|IEMobile|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(
+		ua
+		)
+	) {
+		return "mobile";
+	}
+	return "desktop";
+
+};
+
+// =-=-=-=-=-=-=-=-=-=- </Get-device-type> -=-=-=-=-=-=-=-=-=-=-
+
+
+// =-=-=-=-=-=-=-=-=-=- <image-aspect-ratio> -=-=-=-=-=-=-=-=-=-=-
+
+const imageBody = document.querySelectorAll('.image-body, figure');
+imageBody.forEach(imageBody => {
+	const img = imageBody.querySelector('img'), style = getComputedStyle(imageBody);
+	if(img) {
+		if(img.getAttribute('width') && img.getAttribute('height') && style.position == "relative")
+		imageBody.style.paddingTop = Number(img.getAttribute('height')) / Number(img.getAttribute('width')) * 100 + '%';
+	}
+	
+})
+
+// =-=-=-=-=-=-=-=-=-=- </image-aspect-ratio> -=-=-=-=-=-=-=-=-=-=-
+
+
+
+// =-=-=-=-=-=-=-=-=-=- <click events> -=-=-=-=-=-=-=-=-=-=-
+
+body.addEventListener('click', function (event) {
+
+	function $(elem) {
+		return event.target.closest(elem)
+	}
+
+	// =-=-=-=-=-=-=-=-=-=- <open menu in header> -=-=-=-=-=-=-=-=-=-=-
+
+	if ($('.header__burger')) {
+		menu.forEach(element => {
+			element.classList.toggle('_mob-menu-active')
+		})
+	}
+
+	// =-=-=-=-=-=-=-=-=-=- </open menu in header> -=-=-=-=-=-=-=-=-=-=-
+
+
+
+	// =-=-=-=-=-=-=-=-=-=-=-=- <change-theme> -=-=-=-=-=-=-=-=-=-=-=-=
+	
+	const changeThemeBtn = $(".header__theme--btn")
+	if(changeThemeBtn) {
+	
+		const theme = (localStorage.getItem('security-recipes-theme')) ? localStorage.getItem('security-recipes-theme') : 'light';
+		if(theme == 'light') {
+			html.dataset.theme = 'dark';
+			localStorage.setItem('security-recipes-theme', 'dark');
+		} else {
+			html.dataset.theme = 'light';
+			localStorage.setItem('security-recipes-theme', 'light');
+		}
+	
+	}
+	
+	// =-=-=-=-=-=-=-=-=-=-=-=- </change-theme> -=-=-=-=-=-=-=-=-=-=-=-=
+
+
+
+	// =-=-=-=-=-=-=-=-=-=-=-=- <click> -=-=-=-=-=-=-=-=-=-=-=-=
+	
+	const headerNavButton = $(".header__nav--list .drop-down > button")
+	if(headerNavButton) {
+
+		if(!headerNavButton.classList.contains('active')) {
+			document.querySelectorAll('.header__nav--list .drop-down > button.active').forEach(element => {
+				element.classList.remove('active')
+			})	
+		}
+
+		if(getDeviceType() != 'desktop') {
+			headerNavButton.classList.toggle('active');
+		}
+
+		
+	
+	} else if(getDeviceType() != 'desktop' && !$(".header__nav--list .drop-down")) {
+		document.querySelectorAll('.header__nav--list .drop-down > button.active').forEach(element => {
+			element.classList.remove('active')
+		})
+	}
+	
+	// =-=-=-=-=-=-=-=-=-=-=-=- </click> -=-=-=-=-=-=-=-=-=-=-=-=
+
+	
+	
+
+})
+
+// =-=-=-=-=-=-=-=-=-=- </click events> -=-=-=-=-=-=-=-=-=-=-
+
+
+
+// =-=-=-=-=-=-=-=-=-=-=-=- <resize> -=-=-=-=-=-=-=-=-=-=-=-=
+
+let windowSize = 0;
+
+function resize() {
+
+	html.style.setProperty("--height-header", header.offsetHeight + "px");
+	if(windowSize != window.innerWidth) {
+		html.style.setProperty("--svh", window.innerHeight * 0.01 + "px");
+	}
+	html.style.setProperty("--vh", window.innerHeight * 0.01 + "px");
+	
+
+}
+
+resize();
+
+window.addEventListener('resize', resize)
+
+// =-=-=-=-=-=-=-=-=-=-=-=- </resize> -=-=-=-=-=-=-=-=-=-=-=-=
+
+
+
+// =-=-=-=-=-=-=-=-=-=-=-=- <slider> -=-=-=-=-=-=-=-=-=-=-=-=
+
+if(document.querySelector('.featured__slider')) {
+
+	const featuredSlider = new Splide('.featured__slider', {
+		gap: 15,
+		pagination: false,
+	});
+
+	featuredSlider.mount();
+
+}
+
+// =-=-=-=-=-=-=-=-=-=-=-=- </slider> -=-=-=-=-=-=-=-=-=-=-=-=
+
+
+/* 
+// =-=-=-=-=-=-=-=-=-=-=-=- <lazyload> -=-=-=-=-=-=-=-=-=-=-=-=
+
+new LazyLoad();
+
+// =-=-=-=-=-=-=-=-=-=-=-=- </lazyload> -=-=-=-=-=-=-=-=-=-=-=-=
+ */
+
+
+// =-=-=-=-=-=-=-=-=-=-=-=- <animation> -=-=-=-=-=-=-=-=-=-=-=-=
+
+document.addEventListener('DOMContentLoaded', function() {
+	AOS.init({
+		disable: "mobile",
+		once: true,
+	});
+})
+
+// =-=-=-=-=-=-=-=-=-=-=-=- </animation> -=-=-=-=-=-=-=-=-=-=-=-=
+
---
+++ gulpfile.js
@@ -0,0 +1,295 @@
+
+/*
+
+	gulp start -> start gulp with open in new tab browser
+	gulp -> (default) start gulp WITHOUT open in new tab browser
+
+*/
+
+
+const { src, dest, watch, parallel, series } = require('gulp');
+
+
+const scss			= require('gulp-sass')(require('sass')),
+	  fs			= require('fs'),
+	  concat		= require('gulp-concat'),
+	  avif			= require('gulp-avif'),
+	  webp			= require('gulp-webp'),
+	  imagemin		= require('gulp-imagemin'),
+	  newer			= require('gulp-newer'),
+	  beautify		= require('gulp-beautify'),
+	  browserSync	= require('browser-sync').create(),
+	  uglify		= require('gulp-uglify-es').default,
+	  autoprefixer	= require('gulp-autoprefixer'),
+	  del			= require('del'),
+	  include		= require('gulp-file-include'),
+	  cssbeautify	= require('gulp-cssbeautify'),
+	  minCSS		= require('gulp-cssmin'),
+	  mediaGroup	= require('gulp-group-css-media-queries'),
+	  ttf2woff		= require('gulp-ttf2woff'),
+	  ttf2woff2		= require('gulp-ttf2woff2'),
+	  zipArchive	= require('gulp-zip');
+
+
+
+
+
+function cleanDist() {
+	return del('dist')
+}
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- <Live reload> -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+function browsersync() {
+	browserSync.init({
+		server: {
+			baseDir: 'dist/',
+		},
+		notify: false,
+		open: false,
+	})
+}
+
+function browsersyncStart() {
+	browserSync.init({
+		server: {
+			baseDir: 'dist/',
+		},
+		notify: false,
+	})
+}
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- </Live reload> -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- <images> -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+function images() {
+	return src(['app/img/**/*.*', '!app/img/**/*.webp', '!app/img/**/*.svg'])
+	.pipe(newer('dist/img'))
+	.pipe(avif({ quality: 50 }))
+
+	.pipe(src('app/img/**/*.*'))
+	.pipe(newer('dist/img'))
+	.pipe(webp())
+
+	.pipe(src('app/img/**/*.*'))
+	.pipe(newer('dist/img'))
+	.pipe(imagemin())
+
+
+	.pipe(dest('dist/img'))
+}
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- </images> -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- <Scripts> -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+function scriptsLib() {
+	return src([
+		'node_modules/@splidejs/splide/dist/js/splide.min.js', // Слайдер | npm i @splidejs/splide --save-dev | https://splidejs.com/guides/getting-started/
+		'node_modules/aos/dist/aos.js', // Анимации | npm i aos --save-dev | https://www.npmjs.com/package/aos
+	])
+	.pipe(concat('libs.min.js'))
+	.pipe(uglify())
+	.pipe(dest('dist/js'))
+	.pipe(browserSync.stream())
+}
+
+function scriptsMin() {
+	return src('app/js/main.js')
+	.pipe(concat('main.min.js'))
+	.pipe(uglify())
+	.pipe(dest('dist/js'))
+	.pipe(browserSync.stream())
+}
+
+function scripts() {
+	return src('app/js/main.js')
+	.pipe(scriptsMin())
+	.pipe(dest('dist/js'))
+	.pipe(browserSync.stream())
+}
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- </Scripts> -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- <HTML> -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+function htmlCompilation() {
+	return src(['app/*.html'])
+	.pipe(include())
+	.pipe(beautify.html({ indent_size: 1, indent_char: "	" }))
+	.pipe(dest('dist'))
+	.pipe(browserSync.stream())
+}
+
+function htmlComponents() {
+	return src('app/html/**/_*.html')
+	.pipe(include())
+	.pipe(htmlCompilation())
+}
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- </HTML> -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- <Styles> -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+function styles() {
+	return src('app/scss/style.scss')
+		.pipe(scss({outputStyle: 'compressed'}))
+		.pipe(mediaGroup())
+		.pipe(autoprefixer({
+			overrideBrowserslist: ['last 1 version'],
+			cascade: false
+		}))
+		.pipe(minCSS())
+		.pipe(concat('style.min.css'))
+		.pipe(dest('dist/css'))
+		.pipe(browserSync.stream())
+}
+
+function criticaLStyles() {
+	return src('app/scss/style-critical.scss')
+		.pipe(scss({outputStyle: 'compressed'}))
+		.pipe(mediaGroup())
+		.pipe(autoprefixer({
+			overrideBrowserslist: ['last 1 version'],
+			cascade: false
+		}))
+		.pipe(minCSS())
+		.pipe(concat('style-critical.min.css'))
+		.pipe(dest('dist/css'))
+		.pipe(browserSync.stream())
+}
+
+function stylesOriginal() {
+	return src(['dist/css/style.min.css', 'dist/css/style-critical.min.css'])
+		.pipe(concat('style.css'))
+		.pipe(cssbeautify())
+		.pipe(dest('dist/css'))
+}
+
+function stylesLib() {
+	return src([
+		'node_modules/normalize.css/normalize.css',
+		'node_modules/@splidejs/splide/dist/css/splide.min.css', // Слайдер
+		'node_modules/aos/dist/aos.css', // Анимации
+	])
+	.pipe(concat('_libs.scss'))
+	.pipe(dest('app/scss'))
+}
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- </Styles> -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- <Fonts> -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+function ttf2woffConvert() {
+	return src('app/fonts/*.ttf')
+		.pipe(ttf2woff())
+		.pipe(dest('dist/css'))
+}
+
+function ttf2woff2Convert() {
+	return src('app/fonts/*.ttf')
+		.pipe(ttf2woff2())
+		.pipe(dest('dist/css'))
+}
+
+function fonts() {
+	return src('app/css/*.woff', 'app/css/*.woff2')
+		.pipe(dest('dist/css'))
+}
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- </Fonts> -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+function json() {
+	return src('app/json/*.json')
+		.pipe(dest('dist/json'))
+}
+
+function video() {
+	return src('app/video/*')
+		.pipe(dest('dist/video'))
+}
+
+function audio() {
+	return src('app/audio/*')
+		.pipe(dest('dist/audio'))
+}
+
+let package = fs.readFileSync('package.json'),
+	name;
+
+	package = JSON.parse(package);
+	name = package.name;
+
+function delFolder() {
+	return del(`${name}`)
+}
+
+function createFolder() {
+	return src(['./**', '!./node_modules/**', '!./package-lock.json', `!./${name}.zip`])
+		.pipe(dest(`./${name}`))
+}
+
+function createZip() {
+	return src(`./${name}/**`)
+		.pipe(zipArchive(`${name}.zip`))
+		.pipe(dest(`./`))
+}
+
+function zipDel() {
+	return del(`${name}`);
+}
+
+function sprites() {
+	return src(`app/img/sprites.svg`)
+		.pipe(dest(`dist/img`))
+}
+
+function watching() {
+	watch(['app/scss/**/*.scss'], series(styles, criticaLStyles, stylesOriginal));
+	watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
+	watch(['app/json/*.json'], json);
+	watch(['app/video/*.*'], video);
+	watch(['app/audio/*.*'], audio);
+	watch(['app/img/sprites.svg'], sprites);
+	watch(['app/*.html'], htmlCompilation);
+	watch(['app/html/**/_*.html'], htmlComponents);
+}
+
+exports.images = images;
+exports.styles = styles;
+exports.criticaLStyles = criticaLStyles;
+exports.stylesOriginal = stylesOriginal;
+exports.watching = watching;
+exports.browsersyncStart = browsersyncStart;
+exports.browsersync = browsersync;
+exports.scripts = scripts;
+exports.scriptsMin = scriptsMin;
+exports.scriptsLib = scriptsLib;
+exports.cleanDist = cleanDist;
+exports.stylesLib = stylesLib;
+exports.ttf2woffConvert = ttf2woffConvert;
+exports.ttf2woff2Convert = ttf2woff2Convert;
+exports.fonts = fonts;
+exports.htmlComponents = htmlComponents;
+exports.htmlCompilation = htmlCompilation;
+exports.createFolder = createFolder;
+exports.delFolder = delFolder;
+exports.createZip = createZip;
+exports.zipDel = zipDel;
+
+exports.fonts = series(ttf2woffConvert, ttf2woff2Convert, fonts);
+exports.folder = series(delFolder, createFolder);
+exports.zip = series(createFolder, createZip, zipDel);
+exports.start = parallel(stylesLib, styles, watching, scriptsLib, scriptsMin, scripts, htmlCompilation, json, sprites, browsersyncStart);
+exports.default = parallel(stylesLib, styles, watching, scriptsLib, scriptsMin, scripts, htmlCompilation, json, sprites, browsersync);
